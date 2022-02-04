0cc48a3a4ac89c3d76760bffe3021027f64d3af1
brianmwevi <mwevibrian@gmail.com>
2022-02-05 02:32:46 +0300

    homepage and userpage -- add smooth transition

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const takeQuiz = document.getElementById("take-quiz");
 takeQuiz.addEventListener("click", (e) => {
 	e.preventDefault();
-	document.querySelector(".home").style.display = "none";
-	document.querySelector(".user-detail").style.display = "block";
+	let homePage = document.querySelector(".home");
+	let userPage = document.querySelector(".user-detail");
+	homePage.classList.add("hide");
+	homePage.style.display = "none";
+	userPage.classList.remove("hide");
+	userPage.classList.add("show");
+});
+const userForm = document.querySelector(".user-form");
+userForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	// document.querySelector(".user-detail").style.display = "block";
 });
