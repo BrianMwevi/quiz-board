a3d15e75e60f98f4f19f8603c749d85b9b8981e2
brianmwevi <mwevibrian@gmail.com>
2022-02-05 04:59:48 +0300

    script:user's active page - toggle the active view

--- js/script.js
+++ js/script.js
@@ -1,21 +1,47 @@
 const takeQuiz = document.getElementById("take-quiz");
-const userForm = document.querySelector(".user-form");
+const username = document.querySelector(".user-form");
 
-let homePage = document.querySelector(".home");
-let userPage = document.querySelector(".user-detail");
-let startQuiz = document.querySelector(".start");
+const homePage = document.querySelector(".home");
+const userPage = document.querySelector(".user-detail");
+const startQuiz = document.querySelector(".start");
+const questions = document.querySelector(".questions")
+
+let duration;
+let hours,
+	mins,
+	secs = 0;
+
+// Correct Answers
+let singleSelection = {
+	s1: "HyperText Markup Language",
+	s2: "Cascading Styling Sheet",
+	s3: "Ans-3",
+	s4: "Ans-4",
+};
+let multiSelection = {
+	m1: ["Ans-1", "Ans-2", "Ans-3"],
+	m2: ["Ans-1", "Ans-2", "Ans-3"],
+	m3: ["Ans-1", "Ans-2", "Ans-3"],
+	m4: ["Ans-1", "Ans-2", "Ans-3"],
+};
+let inputQuiz = {
+	i1: "Ans-1",
+	i2: "Ans-2",
+	i3: "Ans-3",
+	i4: "Ans-4",
+};
 takeQuiz.addEventListener("click", (e) => {
 	e.preventDefault();
-    homePage.classList.add("hide");
-    homePage.style.display="none"
+	homePage.classList.add("hide");
+	homePage.style.display = "none";
 	userPage.classList.remove("hide");
 	userPage.classList.add("show");
 });
-userForm.addEventListener("submit", (e) => {
+username.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	startQuiz.classList.remove("hide");
 	startQuiz.classList.add("show");
 	userPage.classList.add("hide");
-    userPage.style.display = "none";
+	userPage.style.display = "none";
 });
