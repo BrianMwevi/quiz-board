caf9a63fad4c7a5a8abb6207a5869a393144c1ee
brianmwevi <mwevibrian@gmail.com>
2022-02-05 04:25:27 +0300

    style & script: full-page layout - add transition

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 const takeQuiz = document.getElementById("take-quiz");
+const userForm = document.querySelector(".user-form");
+
+let homePage = document.querySelector(".home");
+let userPage = document.querySelector(".user-detail");
+let startQuiz = document.querySelector(".start");
 takeQuiz.addEventListener("click", (e) => {
 	e.preventDefault();
-	let homePage = document.querySelector(".home");
-	let userPage = document.querySelector(".user-detail");
-	homePage.classList.add("hide");
-	homePage.style.display = "none";
+    homePage.classList.add("hide");
+    homePage.style.display="none"
 	userPage.classList.remove("hide");
 	userPage.classList.add("show");
 });
-const userForm = document.querySelector(".user-form");
 userForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	// document.querySelector(".user-detail").style.display = "block";
+	startQuiz.classList.remove("hide");
+	startQuiz.classList.add("show");
+	userPage.classList.add("hide");
+    userPage.style.display = "none";
 });
